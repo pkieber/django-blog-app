da9f79efe31f5b2d40252290ef2b6d64fb7fe70d
pkieber <113725311+pkieber@users.noreply.github.com>
2024-03-25 17:39:24 +0100

    show post-detail

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import include, path
-from .views import Index
+from .views import Index, DetailArticleView
 
 urlpatterns = [
     path("tinymce/", include("tinymce.urls")),
     path("", Index.as_view(), name="index"),
+    path("<int:pk>/", DetailArticleView.as_view(), name="detail_article"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views import View
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Article
 
 # Create your views here.
@@ -10,3 +10,6 @@ class Index(ListView):
     template_name = "blog/index.html"
     paginate_by = 1
 
+class DetailArticleView(DetailView):
+    model = Article
+    template_name = "blog/blog_post.html"
