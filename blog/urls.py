b5817f665ad2edca7ae2deae95c5f80c63b37e75
pkieber <113725311+pkieber@users.noreply.github.com>
2024-03-25 19:48:56 +0100

    add like/unlike

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import include, path
-from .views import Index, DetailArticleView
+from .views import Index, DetailArticleView, LikeArticle
 
 urlpatterns = [
     path("tinymce/", include("tinymce.urls")),
     path("", Index.as_view(), name="index"),
     path("<int:pk>/", DetailArticleView.as_view(), name="detail_article"),
+    path("<int:pk>/like/", LikeArticle.as_view(), name="like_article"),
 ]
