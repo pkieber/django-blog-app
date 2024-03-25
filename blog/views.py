1dd85e6d995dc20043b1b0beda85ed26367628df
pkieber <113725311+pkieber@users.noreply.github.com>
2024-03-25 16:58:25 +0100

    show posts

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.views import View
+from django.views.generic import ListView
+from .models import Article
 
 # Create your views here.
-class Index(View):
-    def get(self, request):
-        return render(request, "blog/index.html")
-    
+class Index(ListView):
+    model = Article
+    queryset = Article.objects.all().order_by("-date")
+    template_name = "blog/index.html"
+
